16ca1093a0cb01807ea9e309db3d5dcb0cbf2389
Marta Tabor <leeloothefifth@googlemail.com>
2024-02-07 20:37:58 +0000

    task-01-add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const background = document.querySelector('body');
 
+let colorTimer = null;
+
+let randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+btnStart.addEventListener('click', () => {
+  colorTimer = setInterval(() => {
+    background.style.backgroundColor = randomColor();
+  }, 1000);
+
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(colorTimer);
+  btnStart.disabled = false;
+});
